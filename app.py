dfba17ecc66f32573f513691467e3cea09c2cd4a
Shiva <sh.ramezani94@gmail.com>
2024-09-29 10:39:06 +0330

    Part3: train cancer detection model

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_wtf import FlaskForm
+from wtforms import FloatField, SubmitField, StringField
+from wtforms.validators import DataRequired
+import numpy as np
+
+from ml.predict import predict_cancer
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'your_secret_key' 
+
+
+
+class PredictionForm(FlaskForm):
+    worst_area = FloatField('Worst Area', default=2019.0, validators=[DataRequired()])
+    worst_concave_points = FloatField('Worst Concave Points', default=0.2654, validators=[DataRequired()])
+    mean_concave_points = FloatField('Mean Concave Points', default=0.14710, validators=[DataRequired()])
+    worst_radius = FloatField('Worst Radius', default=25.380, validators=[DataRequired()])
+    mean_concavity = FloatField('Mean Concavity', default=0.30010, validators=[DataRequired()])
+    submit = SubmitField('Predict')
+
 
 @app.route('/')
 def home():
@@ -26,10 +45,23 @@ def logout():
 
 @app.route('/user_input', methods=['GET', 'POST'])
 def user_input():
-    if request.method == 'POST':
-        pass
+    form = PredictionForm()
+    prediction_text = "NO PREDICTION"
+    
+    if form.validate_on_submit():
+        features = np.array([[
+            form.worst_area.data,
+            form.worst_concave_points.data,
+            form.mean_concave_points.data,
+            form.worst_radius.data,
+            form.mean_concavity.data
+        ]])
+        prediction = predict_cancer(features)[0]
+        cancer_type = 'Malignant' if prediction == 1 else 'Benign'
+        prediction_text=f'The tumor is {cancer_type}'
+        
+    return render_template('user_input.html', form=form, prediction=prediction_text)
 
-    return render_template('user_input.html')
 
 @app.route('/result')
 def result():
@@ -38,6 +70,7 @@ def result():
 @app.route('/history')
 def history():
     return render_template('history.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
